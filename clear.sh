3f8cf6221217327cbd07b530bd4ba5c69f3e38a6
Yury Litvinov <litv-exp@lxir131.gsi.de>
2024-01-18 12:48:05 +0100

    modify some files and make a video

--- clear.sh
+++ clear.sh
@@ -1,4 +1,5 @@
-rm -rf build *~ *.egg-info Analyze_iq_sc/__pycache__/ Analyze_iq_sc/*~
+rm -rf build *~ *.egg-info analyze_iq_sc/__pycache__/ analyze_iq_sc/*~
 git rm synced_files_* -r
 git rm analyzed_files_* -r
 git rm analyze_iq_sc/__pycache__ -r
+
